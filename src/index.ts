c13544ccf373e931f585de7aa3d0b62be1c3a2b0
Paolo Vincent Julian <paolo.julian@accelo.com>
2025-02-16 17:41:02 +0800

    feat: add example to using observer pattern

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import IdleChecker from './utils/IdleChecker.js';
 
 function onDOMContentLoaded() {
   console.log('isIdle default value', IdleChecker.isIdle.getValue());
+  IdleChecker.isIdle.subscribe((newValue) => {
+    console.log('a new value!', newValue);
+  });
+
+  IdleChecker.isIdle.setValue(true)
 }
 
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
