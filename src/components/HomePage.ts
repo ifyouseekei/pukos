4245bdb36ebd63369740629ad94a4e627b4bd40d
Paolo Vincent Julian <paolo.julian@accelo.com>
2025-02-18 00:41:57 +0800

    refactor: improve HomePage components

--- src/components/HomePage.ts
+++ src/components/HomePage.ts
@@ -1,7 +1,4 @@
-import IntervalService, {
-  Intervals,
-  INTERVALS_ENUM,
-} from '../services/IntervalService/IntervalService.js';
+import IntervalService from '../services/IntervalService/IntervalService.js';
 import PomodoroService from '../services/PomodoService/PomodoroService.js';
 import { getOrThrowElement } from '../utils/getOrThrowElement.js';
 import { secondsToTimeString } from '../utils/secondsToTimeString.js';
@@ -24,21 +21,26 @@ class HomePage {
   }
 
   init() {
-    this.focusButtonEl.addEventListener(
-      'click',
-      this.handleFocusClick.bind(this)
+    this.initButtons();
+    this.initIntervals();
+    this.initCountdownTimer();
+  }
+
+  private initButtons(): void {
+    this.focusButtonEl.addEventListener('click', () =>
+      PomodoroService.onFocus()
     );
 
-    this.takeABreakButtonEl.addEventListener(
-      'click',
-      this.handleBreakClick.bind(this)
+    this.takeABreakButtonEl.addEventListener('click', () =>
+      PomodoroService.onBreak()
     );
 
-    this.endSessionButtonEl.addEventListener(
-      'click',
-      this.handleEndSessionClick.bind(this)
+    this.endSessionButtonEl.addEventListener('click', () =>
+      PomodoroService.onReset()
     );
+  }
 
+  private initIntervals(): void {
     // Initialize interval value
     this.intervalEls.forEach((intervalEl) => {
       if (intervalEl.value === IntervalService.interval.getValue()) {
@@ -48,50 +50,33 @@ class HomePage {
 
     // Listen to interval change
     this.intervalEls.forEach((intervalEl) => {
-      intervalEl.addEventListener(
-        'change',
-        this.handleIntervalChange.bind(this)
-      );
-    });
+      intervalEl.addEventListener('change', (event) => {
+        const target = event.target as HTMLInputElement;
+        if (!target.checked) {
+          return;
+        }
 
+        // Check
+        const selectedInterval = target.value;
+        if (!IntervalService.isIntervalValid(selectedInterval)) {
+          return;
+        }
+
+        IntervalService.setInterval(selectedInterval);
+      });
+    });
+  }
+
+  private initCountdownTimer(): void {
+    // Set the initial value
     this.countdownTimerEl.textContent = secondsToTimeString(
       PomodoroService.remainingTime.getValue()
     );
 
-    PomodoroService.remainingTime.subscribe(
-      this.handleRemainingTimeChange.bind(this)
-    );
-  }
-
-  handleFocusClick() {
-    PomodoroService.onFocus();
-  }
-
-  handleBreakClick() {
-    PomodoroService.onBreak();
-  }
-
-  handleEndSessionClick() {
-    PomodoroService.onReset();
-  }
-
-  handleRemainingTimeChange(remainingTime: number) {
-    this.countdownTimerEl.textContent = secondsToTimeString(remainingTime);
-  }
-
-  handleIntervalChange(event: Event) {
-    const target = event.target as HTMLInputElement;
-    if (!target.checked) {
-      return;
-    }
-
-    // Check
-    const selectedInterval = target.value;
-    if (!isIntervalValid(selectedInterval)) {
-      return;
-    }
-
-    IntervalService.setInterval(selectedInterval);
+    // Listen to changes on remaining time to reflex to the countdownTimer
+    PomodoroService.remainingTime.subscribe((remainingTime) => {
+      this.countdownTimerEl.textContent = secondsToTimeString(remainingTime);
+    });
   }
 
   public static getInstance(): HomePage {
@@ -101,15 +86,6 @@ class HomePage {
 
     return this.instance;
   }
-}
-
-const validIntervals = Object.keys(INTERVALS_ENUM);
-function isIntervalValid(value?: unknown): value is Intervals {
-  if (typeof value !== 'string') {
-    return false;
-  }
-
-  return validIntervals.includes(value as Intervals);
 }
 
 export default HomePage.getInstance();
