9825a013e43f3dbb5eada7971d39f00facf45ffe
Paolo Vincent Julian <paolo.julian@accelo.com>
2025-02-25 19:42:54 +0800

    doc: update doc for tests

--- src/services/IntervalService/IntervalService.test.ts
+++ src/services/IntervalService/IntervalService.test.ts
@@ -1,43 +1,46 @@
-import { LOCAL_STORAGE_KEY, IntervalService } from './IntervalService.js';
+import { LOCAL_STORAGE_KEY, IntervalService } from "./IntervalService.js";
 
-describe('IntervalService', () => {
+describe("IntervalService", () => {
   afterEach(() => {
     localStorage.clear();
     (IntervalService as any).instance = null;
   });
 
-  describe('initializing', () => {
-    test('should have default values', () => {
+  describe("initializing", () => {
+    test("should have default values", () => {
       const intervalService = IntervalService.getInstance();
-      expect(intervalService.interval.getValue()).toBe('25:5');
-    });
-
-    test('should load saved interval from localStorage', () => {
-      localStorage.setItem(LOCAL_STORAGE_KEY, '90:30');
-      const instance = IntervalService.getInstance();
-      expect(instance.interval.getValue()).toBe('90:30');
+      expect(intervalService.interval.getValue()).toBe("25:5");
     });
   });
 
-  describe('changing interval', () => {
-    test('should not allow invalid intervals', () => {
+  describe("initializing when there is a stored interval from local storage", () => {
+    test("should load saved interval from localStorage as the default interval", () => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, "90:30");
+      const instance = IntervalService.getInstance();
+      expect(instance.interval.getValue()).toBe("90:30");
+    });
+  });
+
+  describe("changing interval", () => {
+    test("should not allow invalid intervals", () => {
       const instance = IntervalService.getInstance();
 
       // @ts-expect-error - intentionally passing an invalid value
-      instance.setInterval('invalid');
-      expect(instance.interval.getValue()).toBe('25:5'); // Default remains unchanged
+      instance.setInterval("invalid");
+      expect(instance.interval.getValue()).toBe("25:5"); // Default remains unchanged
     });
-    test('should be able to change and store to localStorage', () => {
+
+    test("should be able to change and store to localStorage", () => {
       const instance = IntervalService.getInstance();
 
-      expect(instance.interval.getValue()).toBe('25:5');
-      instance.setInterval('50:10');
-      expect(instance.interval.getValue()).toBe('50:10');
-      expect(localStorage.getItem(LOCAL_STORAGE_KEY)).toBe('50:10');
+      expect(instance.interval.getValue()).toBe("25:5");
+      instance.setInterval("50:10");
+      expect(instance.interval.getValue()).toBe("50:10");
+      expect(localStorage.getItem(LOCAL_STORAGE_KEY)).toBe("50:10");
 
-      instance.setInterval('90:30');
-      expect(instance.interval.getValue()).toBe('90:30');
-      expect(localStorage.getItem(LOCAL_STORAGE_KEY)).toBe('90:30');
+      instance.setInterval("90:30");
+      expect(instance.interval.getValue()).toBe("90:30");
+      expect(localStorage.getItem(LOCAL_STORAGE_KEY)).toBe("90:30");
     });
   });
 });
